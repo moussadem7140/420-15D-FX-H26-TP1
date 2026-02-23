1ac8dcaf7578e05a56d2a1369528213888881a48
Moussa Dembélé <benoking190@gmail.com>
2026-02-22 19:32:02 -0500

    Ajustement de la participation

--- Pages/Participation/Participer.cshtml.cs
+++ Pages/Participation/Participer.cshtml.cs
@@ -1,5 +1,6 @@
 using _420_15D_FX_H26_TP1.Data;
 using _420_15D_FX_H26_TP1.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -7,7 +8,7 @@ using System.Linq;
 
 namespace _420_15D_FX_H26_TP1.Pages.Participation
 {
-
+    [Authorize]
     public class ParticiperModel : PageModel
     {
         private readonly ApplicationDbContext _context;
@@ -16,21 +17,22 @@ namespace _420_15D_FX_H26_TP1.Pages.Participation
             _context = context;
         }
         public Guid EvenementId { get; set; }
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnPost(Guid Id)
         {
             _context.Participations.Add(new _420_15D_FX_H26_TP1.Models.Participation
             {
                 Id = Guid.NewGuid(),
-                EvenementId = EvenementId,
-                UtilisateurId = User.Identity.Name
+                EvenementId = Id,
+                UtilisateurId = _context.Users.FirstOrDefault(u => u.UserName == User.Identity.Name).Id
             });
              _context.SaveChanges();
             TempData["SuccessMessage"] = "Vous avez participé à l'événement avec succès !";
             return RedirectToPage("/index");
         }
-        public async Task<IActionResult> OnGetAnnuler(Guid evenementId)
+        public async Task<IActionResult> OnPostAnnuler(Guid Id)
         {
-            var P = await _context.Participations.FirstOrDefaultAsync(p => p.EvenementId == evenementId && p.UtilisateurId == User.Identity.Name);
+            var P = await _context.Participations.FirstOrDefaultAsync(p => p.EvenementId == Id && p.Utilisateur.UserName == User.Identity.Name);
+
                 _context.Participations.Remove(P);
                 await _context.SaveChangesAsync();
                 TempData["SuccessMessage"] = "Votre participation à l'événement a été annulée avec succès !";
